744cd9eaffe425e82d4a73d8021863514bc25951
rockbell-E <jonathanilllanes@gmail.com>
2025-03-02 20:31:35 -0300

    Se modifico el server.js para que haga el uso de ejs, ocupe el views para renderlizar las paginas y se agrego el public para hacer uso de css, img y js. Ademas se cambio todos los html a ejs para poder heredar el header y el footer

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, '../public')))
+
 
 app.get("/", (req, res) => {
-    res.send("Conexion exitosa");
-})
+    res.render("index");
+});
 
-app.listen(3000, () => {
-    console.log("server escuchando en el puerto 3000");
-})
+app.get("/iniciar-sesion", (req, res) => {
+    res.render("log-in");
+});
+
+app.get("/sobre-nosotros", (req, res) => {
+    res.render("about-us");
+});
+
+app.get("/registrarse", (req, res) => {
+    res.render("register");
+});
+app.get("/recuperar", (req, res) => {
+    res.render("recovery");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
