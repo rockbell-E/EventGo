42661389b12dc6ff6930021cba56222696c9cc80
rockbell-E <jonathanilllanes@gmail.com>
2025-03-10 11:35:53 -0300

    Se agrego el body parser al servidor y se agrego el enctype al formulario

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require('body-parser');
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 app.use(express.static(path.join(__dirname, '../public')))
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.render("index");
