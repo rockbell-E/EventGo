2d53216630dd6b161db48ede118ecf56f498af6f
rockbell-E <jonathanilllanes@gmail.com>
2025-08-02 12:30:17 -0400

    Actualizar server.js

--- server/server.js
+++ server/server.js
@@ -28,6 +28,5 @@ app.get("/recuperar", (req, res) => {
     res.render("recovery");
 });
 
-
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
